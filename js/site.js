a4084f269509982b29278465f88e945bba77407b
Jeff Chambers <jeff@highrankwebsites.com>
2017-01-31 11:22:17 -0800

    site.js - slide toggles for menus, match heights, slick carousels, and smooth scrolling

--- js/site.js
+++ js/site.js
@@ -1,13 +1,59 @@
-jQuery(document).ready(function(){
-    // MAIN NAV MENU TOGGLE
+jQuery(document).ready(function () {
     jQuery(".menu-toggle").click(function ($) {
         jQuery(this).toggleClass("open");
-        jQuery('#menu-header').toggleClass("open");
+        jQuery('#menu-header').slideToggle("slow");
     });
-    // SUB-MENU TOGGLES
     jQuery(".menu-item-has-children").click(function ($) {
-        jQuery(this).toggleClass("open");
-        jQuery(this).children('.sub-menu').toggleClass("open");
+        jQuery(this).children('.sub-menu').slideToggle("slow");
     });
-}); 
-
+    jQuery(".menu-item-has-children").hover(function ($) {
+        jQuery(this).children('.sub-menu').slideToggle();
+    });
+    jQuery(".banner > a").click(function (event) {
+        event.preventDefault();
+        jQuery.smoothScroll({scrollTarget: (this).hash});
+    });
+    jQuery('select').niceSelect();
+    jQuery('.column').matchHeight();
+    jQuery('.sub-column').matchHeight();
+    jQuery('.employee').matchHeight();
+    jQuery('#menu-practice-areas li').matchHeight();
+    jQuery('.item').matchHeight();
+    jQuery('.practice-areas .item').matchHeight();
+    jQuery(".selling-points-carousel").slick({infinite: false, dots: false, arrows: true, slidesToShow: 3, slidesToScroll: 1, responsive: [
+        {breakpoint: 1199, settings: {slidesToShow: 1, infinite: true}}
+    ]});
+    jQuery(".practice-areas-carousel").slick({infinite: true, dots: false, arrows: true, slidesToShow: 7, slidesToScroll: 1, responsive: [
+        {breakpoint: 1550, settings: {slidesToShow: 5, infinite: true}},
+        {breakpoint: 1199, settings: {slidesToShow: 3, infinite: true}},
+        {breakpoint: 767, settings: {slidesToShow: 1, infinite: true}}
+    ]});
+    jQuery(".awards-carousel").slick({infinite: true, dots: false, arrows: true, slidesToShow: 4, slidesToScroll: 1, responsive: [
+        {breakpoint: 1199, settings: {slidesToShow: 3, infinite: true, dots: true}},
+        {breakpoint: 767, settings: {slidesToShow: 1, infinite: true, dots: true}}
+    ]});
+    jQuery(function ($) {
+        jQuery('a[href*="#more"]:not([href="#"])').click(function ($) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = jQuery(this.hash);
+                target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    jQuery('html, body').animate({scrollTop: target.offset().top}, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    jQuery(function ($) {
+        jQuery('a[href*="#form"]:not([href="#"])').click(function ($) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = jQuery(this.hash);
+                target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    jQuery('html, body').animate({scrollTop: target.offset().top}, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+});
