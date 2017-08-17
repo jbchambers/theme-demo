cc390e4d72ff8d46464659d8989a2c7ce6fdb26b
Jeff Chambers <jeffreybryanchambers@gmail.com>
2017-08-17 12:24:27 -0700

    fixed default 404 error page

--- 404.php
+++ 404.php
@@ -1,12 +1,17 @@
-<?php get_header();?>
-
-
-	<div id="page-container" class="clearfix">
-		<div class="main-content full-width">
-			<p class="404">404</p>
-			<p>The page you were looking for appears to have been moved, deleted or does not exist. You could <a class="go-back" onclick="history.back();" href="#">go back</a> to where you were or head straight to our <a href="<?php echo site_url();?>">home page</a></p>
-		</div>
-	</div>
-
-
-<?php get_footer();?>
+<?php get_header(); ?>
+<div class="page-content clearfix">
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="main-content column">
+      <h1><?php the_title(); ?></h1>
+      <p class="404">404</p>
+      <p>The page you were looking for appears to have been moved, deleted or does not exist. You could <a class="go-back" onclick="history.back();" href="#">go back</a> to where you were or head straight to our <a href="<?php echo site_url();?>">home page</a></p>
+    </div>
+    <div class="sidebar column">
+      Sidebar
+    </div>
+  <?php endwhile;
+  else : ?>
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+  <?php endif; ?>
+</div>
+<?php get_footer(); ?>
